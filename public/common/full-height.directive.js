ac5d062db6911a3f0a7dd415693ac135dfe8fa55
Gaidukov Taras <kemaweyan@gmail.com>
2017-08-27 19:20:18 +0300

    Added fullHeight directive

---
+++ public/common/full-height.directive.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular
+  .module('expressChat')
+  .directive('fullHeight', ['$window',
+    function ($window) {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attrs) {
+                setHeight(element, $window.innerHeight);
+                angular.element($window).bind('resize', function() {
+                    setHeight(element, $window.innerHeight);
+                });
+            }
+        };
+
+        function setHeight(element, height) {
+            element.css("height", height + "px");
+        }
+    }
+]);
