d84a6c60714955e853231293374aeb97b1944c58
Gaidukov Taras <kemaweyan@gmail.com>
2017-08-29 18:09:41 +0300

    Added profile panel

--- public/profile-panel/profile-panel.component.js
+++ public/profile-panel/profile-panel.component.js
@@ -4,9 +4,12 @@ angular
   .module('profilePanel')
   .component('profilePanel', {
     templateUrl: 'profile-panel/profile-panel.template.html',
-    controller: ['$location', 'Auth',
-        function ($location, Auth) {
+    controller: ['$location', 'Auth', 'User',
+        function ($location, Auth, User) {
             var self = this;
+
+            self.userName = User.firstName + " " + User.lastName;
+            self.avatar = User.avatar ? "/images/avatars/100/" + User.avatar : "/images/100/no-avatar.png";
 
             self.logout = function () {
                 Auth.logout().then(function (resp) {
