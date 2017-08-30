c16d874226a6a10bbd0f9f783ff4465019244ef7
Gaidukov Taras <kemaweyan@gmail.com>
2017-08-30 23:54:22 +0300

    Added chat layout

--- public/chat/chat.component.js
+++ public/chat/chat.component.js
@@ -7,6 +7,8 @@ angular
     controller: ['$routeParams', 'Chat',
         function ($routeParams, Chat) {
             var self = this;
+            self.buddyName = "Mohamed Djudaev";
+            self.buddyAvatar = "/images/100/no-avatar.png";
 
             Chat.setActiveChat($routeParams.chatId);
         }
