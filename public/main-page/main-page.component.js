0746b5e0d52aadf1db8fb0ecd640056b73092c09
Gaidukov Taras <kemaweyan@gmail.com>
2017-09-03 01:29:29 +0300

    Added main page

--- public/main-page/main-page.component.js
+++ public/main-page/main-page.component.js
@@ -4,7 +4,19 @@ angular
   .module('mainPage')
   .component('mainPage', {
     templateUrl: 'main-page/main-page.template.html',
-    controller: function () {
-        
-    }
+    controller: ['User', 'ChatList', '$location',
+        function (User, ChatList, $location) {
+            var self = this;
+            self.user = User;
+            self.chats = ChatList.chats;
+
+            self.search = function () {
+                $location.path('/search/' + self.query);
+            };
+
+            self.openChat = function (buddyId) {
+                $location.path('/chat/' + buddyId);
+            };
+        }
+    ]
 });
